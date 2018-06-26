142c56dac902ff000499778bf27192d56d1b6b0e
Roy Lambreghs <roy.lambreghs@hetic.net>
2018-06-26 14:47:20 +0200

    Login update
    Complete loginsystem + searchengine
    
    #Issue : Article selected after research not displaying in article.php

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 include 'connect.php';
 ?>
 
@@ -16,14 +18,49 @@ include 'connect.php';
                 <li><a href="index.php">Accueil</a></li>
             </ul>
             <div class="nav-login">
-                <form>
-                    <input type="text" name="uid" placeholder="Utilisateur/e-mail">
-                    <input type="password" name="pwd" placeholder="Mot de passe">
-                    <button type="submit" name="submit">Connection</button>
-                </form>
-                <a href="signup.php">S'inscrire</a>
+                <?php
+                    if (isset($_SESSION['u_id'])) {
+                        echo '<form action="includes/logout.inc.php" method="POST">
+                              <button type="submit" name="submit">Déconnexion</button>
+                              </form>';
+                    } else {
+                        echo'<form action="includes/login.inc.php" method="POST">
+                             <input type="text" name="uid" placeholder="Utilisateur/e-mail">
+                             <input type="password" name="pwd" placeholder="Mot de passe">
+                             <button type="submit" name="submit">Connection</button>
+                             </form>
+                             <a href="signup.php">Inscription</a>';
+                    }
+                ?>
             </div>
         </div>
     </nav>
 </header>
 <body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- signup.php
+++ signup.php
@@ -1,13 +1,42 @@
 <?php
-include 'header.php';
+include_once 'header.php';
 ?>
 
     <section class="main-container">
         <div class="main-wrapper">
-            <h2>Trous Noir</h2>
+            <h2>Inscription</h2>
+            <form class="signup-form" action="includes/signup.inc.php" method="POST">
+                <input type="text" name="first" placeholder="Prénom">
+                <input type="text" name="last" placeholder="Nom">
+                <input type="text" name="email" placeholder="E-mail">
+                <input type="text" name="uid" placeholder="Nom d'utilisateur">
+                <input type="password" name="pwd" placeholder="Mot de passe">
+                <button type="submit" name="submit">Confirmer</button>
+            </form>
         </div>
     </section>
 
 <?php
 include "footer.php"
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
